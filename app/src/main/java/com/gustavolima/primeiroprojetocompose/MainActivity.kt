8574ac08a571eae96d52ae7cf00a17a322a86055
Gustavo Lima <gustavo.f12@hotmail.com>
2023-08-10 22:44:10 -0300

    Sample code using Compose implemented.

--- app/src/main/java/com/gustavolima/primeiroprojetocompose/MainActivity.kt
+++ app/src/main/java/com/gustavolima/primeiroprojetocompose/MainActivity.kt
@@ -13,12 +13,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,30 +38,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PrimeiroProjetoComposeTheme {
-                Greeting(name = "teste")
+                var count by remember {
+                    mutableStateOf(0)
+                }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Text(
+
+                        text = count.toString(),
+                        fontSize = 16.sp
+                    )
+                    Button(onClick = {
+
+                        count++
+                    }) {
+                        Text(text = "Click-me")
+                    }
+
+                }
+
             }
 
         }
     }
-
-    @Composable
-    fun Greeting(name: String) {
-       Image(painterResource(id = R.drawable.ic_launcher_foreground), contentDescription =null,
-           modifier = Modifier.background(Color.Black))
-
-    }
-
-    @Preview(showBackground = true)
-    @Composable
-    fun DefaultPreview() {
-        PrimeiroProjetoComposeTheme {
-
-            Greeting(name = "Gustavo")
-
-        }
-    }
-
-
 }
+
+
 
 
